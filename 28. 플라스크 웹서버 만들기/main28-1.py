450cf8a0b6439550defdefb5bfacd7ee5a7360d1
seoyeon <happynew11@naver.com>
2022-11-28 17:21:15 +0900

    28. 플라스크 웹서버 만들기

---
+++ 28. 플라스크 웹서버 만들기/main28-1.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "hello"
+
+def main():
+    app.run(debug = True, port=80)
+    
+if __name__ == '__main__':
+    main()
+    
